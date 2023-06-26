c70717226285390a0c249de76c8113b9f8056771
1Joysara <fionaomanyo@gmail.com>
2023-06-26 14:17:38 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -28,7 +28,7 @@ if (s[i] >= '0' && s[i] <= '9')
 digit = s[i] - '0';
 if (d % 2)
 	digit = -digit;
-n = (n * 10) + digit;
+n = n * 10 + digit;
 f = 1;
 if (s[i + 1] < '0' || s[i + 1] > '9')
 	break;
@@ -37,7 +37,7 @@ f = 0;
 i++;
 }
 if (f == 0)
-	return (0);
-
 	return (n);
+
+	return (0);
 }
