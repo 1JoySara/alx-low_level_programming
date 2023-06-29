4b3069aa5fc69ae839e5e85dfec5348c5ed22d77
1Joysara <fionaomanyo@gmail.com>
2023-06-29 13:45:34 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,6 @@
  * _strcmp - Compares two strings using putchar
  * @s1: First string
  * @s2: Second string
- *
  * Return: 0 if the strings are equal, negative if s1 < s2, positive if s1 > s2
  */
 int _strcmp(char *s1, char *s2)
