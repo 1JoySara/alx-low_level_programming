5b9b7d850caf114d83caa82f98149debf9191c21
1Joysara <fionaomanyo@gmail.com>
2023-06-29 13:26:07 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+/**
+ * _strncpy - Copies a string using putchar
+ * @dest: Destination buffer
+ * @src: Source string
+ * @n: Maximum number of characters to copy
+ *
+ * Return: Pointer to the destination string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+putchar(dest[i]);
+}
+
+for (; i < n; i++)
+{
+dest[i] = '\0';
+putchar(dest[i]);
+}
+
+return (dest);
+}
